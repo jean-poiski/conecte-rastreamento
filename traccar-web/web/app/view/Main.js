a74d80ca7bf1f1184497d793d63af3482aedb4c4
jeanpoiski <jpoiski@gmail.com>
2020-01-07 20:55:38 -0300

    Commit inicial do projeto

---
+++ traccar-web/web/app/view/Main.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 - 2017 Anton Tananaev (anton@traccar.org)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Ext.define('Traccar.view.Main', {
+    extend: 'Ext.container.Viewport',
+    alias: 'widget.main',
+
+    requires: [
+        'Traccar.view.MainController',
+        'Traccar.view.edit.Devices',
+        'Traccar.view.State',
+        'Traccar.view.Report',
+        'Traccar.view.Events',
+        'Traccar.view.map.Map'
+    ],
+
+    controller: 'mainController',
+
+    layout: 'border',
+    componentCls: 'toolbar-header-style',
+
+    defaults: {
+        collapsible: true,
+        split: false
+    },
+
+    items: [{
+        region: 'west',
+        xtype: 'panel',
+        layout: 'accordion',
+        width: Traccar.Style.deviceWidth,
+        title: 'Painel de Gerenciamento',
+        floatable: true,
+        stateful: true,
+        collapsed: true,
+        stateId: 'devices-and-state-and-events-panel',
+        componentCls: 'toolbar-header-style',
+
+        defaults: {
+            header: true,
+            flex: 1
+        },
+
+        items: [{
+            title: Strings.deviceTitle,
+            xtype: 'devicesView',
+            reference: 'devicesView'
+        }, {
+            title: Strings.stateTitle,
+            xtype: 'stateView',
+            reference: 'stateView'
+        }, {
+            xtype: 'eventsView',
+            reference: 'eventsView'
+        }, {
+            region: 'center',
+            xtype: 'reportView',
+            reference: 'reportView'
+        }]
+    }, {
+        region: 'center',
+        xtype: 'mapView',
+        collapsible: false,
+        header: false,
+        margin: '0 0 0 2'
+    }]
+});
